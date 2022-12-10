28701472a402a8986ce98677cb2511460939e662
JaeHoon <87574833+tlshoon@users.noreply.github.com>
2022-12-10 21:45:12 +0900

    Add files via upload

--- Dynamic Programming.py
+++ Dynamic Programming.py
@@ -61,10 +61,10 @@
 #
 # for i in range(2,x+1):
 #     # 현재의 수에서 1을 빼는 경우
-#     d[i] = d[i-1] + 1
+#     d[i] = d[i-1] + 1   # 이전 값에 +1 하는 경우 (기본 정의)
 #     # 현재의 수가 2로 나누어 떨어지는 경우
 #     if i % 2 == 0:
-#         d[i] = min(d[i],d[i//2] + 1)
+#         d[i] = min(d[i],d[i//2] + 1)  # 이전 값 + 1, 2로 나눈 값 (이전까지 세어진 값) + 1 중에 최소값
 #     # 현재의 수가 3으로 나누어 떨어지는 경우
 #     if i % 3 == 0:
 #         d[i] = min(d[i], d[i//3] + 1)
@@ -73,3 +73,28 @@
 #         d[i] = min(d[i], d[i//5] + 1)
 # print(d[x])
 
+# 개미전사
+# n = int(input())
+# array = list(map(int,input().split()))
+#
+# d = [0] * 100 # 앞서 계산된 결과를 저장하기 위한 DP테이블 초기화
+#
+# d[0] = array[0]
+# d[1] = max(array[0],array[1])
+#
+# for i in range(2,n):
+#     d[i] = max(d[i-1],d[i-2] + array[i])
+#
+# print(d[n-1])
+
+# 바닥 공사
+# n = int(input())
+#
+# d = [0] * 1001
+#
+# d[1] = 1
+# d[2] = 3
+#
+# for i in range(3,n+1):
+#     d[i] = (d[i-1] + 2 * d[i-2]) % 796796
+# print(d[n])
